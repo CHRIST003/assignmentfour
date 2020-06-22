c4888ea7d2b70a6af257fa546e12c7430f004696
CHRIST003 <christnganga06@gmail.com>
2020-06-23 00:07:39 +0200

    used of List, HashSet and HashMap. there are all subclasses of collection

--- src/test/java/ac/za/cput/AppTest.java
+++ src/test/java/ac/za/cput/AppTest.java
@@ -2,13 +2,21 @@ package ac.za.cput;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
+
+    private Animal animal = new Animal("milou","dog");
+    private Animal cat = new Animal("bobi","cat");
+    App app = new App();
     /**
      * Rigorous Test :-)
      */
@@ -16,5 +24,16 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void readList() {
+        List<Animal> ani = new ArrayList<>();
+        ani.add(animal);
+        ani.add(cat);
+
+        Assert.assertTrue(app.readList(ani));
+        System.out.println(app.getAnimal("milou"));
+
     }
 }
